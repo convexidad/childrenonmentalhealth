dc79c79ab92aff9e75b1cac9fdff9cdb7617bbd9
Aoi Montecinos <aoimontecinos@gmail.com>
2022-11-30 14:52:22 -0300

    primer commit

---
+++ codigo.R
@@ -0,0 +1,20 @@
+library(elsoc)
+load_elsoc()
+
+
+############CREACIÓN DE VARIABLES
+
+
+
+
+
+
+
+################## ESTADÍSTICA DESCRIPTIVA
+
+
+
+
+
+
+############ MODELOS ECONOMÉTRICOS Y PSICOMÉTRICOS
